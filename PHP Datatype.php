004a1c9fa7540b74b89d57ba0de1215305f696bd
Faisal Shahzad <55628771+FaisalShahzadCS@users.noreply.github.com>
2022-09-12 16:24:45 +0500

    Add files via upload

---
+++ PHP Datatype.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Variables</title>
+  </head>
+  <body>
+    <?php
+    $name="Muhammad Faisal Shahzad<br>";
+    echo $name;
+    var_dump($name);
+     ?>
+  </body>
+</html>
---
+++ PHP Variables.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Variables</title>
+  </head>
+  <body>
+    <?php
+    $name="Muhammad Faisal Shahzad<br>";
+    echo $name;
+    $name="Muhammad Faisal Shahzad<br>";
+    echo "<h1>". $name ."<h1>";
+     ?>
+  </body>
+</html>
