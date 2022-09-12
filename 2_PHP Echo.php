e9366e533ac36311c9811bd7ea88cc06575fe80b
Faisal Shahzad <55628771+FaisalShahzadCS@users.noreply.github.com>
2022-09-12 15:59:28 +0500

    Add files via upload

---
+++ 2_PHP Echo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Echo</title>
+  </head>
+  <body>
+    <?php
+    echo "Faisal Shahzad<br>";
+    echo "<b>Faisal Shahzad</b><br>";
+    print "Faisal Shahzad<br>";
+
+
+     ?>
+  </body>
+</html>
